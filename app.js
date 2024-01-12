66f7f1b841db2995001e2c4074461c81291de6d1
LeCheWang <haphonggoroki@gmail.com>
2024-01-12 15:34:02 +0700

    add cors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const connectDB = require("./configs/database");
 const router = require("./routers");
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
